2895db8129f4a2a114e3f50e73118f2a6cd51b92
Your Name <you@example.com>
2017-03-14 08:56:09 +0200

    added routing

---
+++ api/v1_0/routing/Issue.php
@@ -0,0 +1,19 @@
+<?php
+use api\v1_0\controller\Issue\IssueController;
+
+use \rest\router;
+use \rest\request;
+
+router::addController('', IssueController::class);
+
+router::controller(IssueController::class)
+	->addAction(request::HTTP_POST, 'issue', 'create');
+
+router::controller(IssueController::class)
+	->addAction(request::HTTP_GET, 'issue', 'get');
+
+router::controller(IssueController::class)
+	->addAction(request::HTTP_PUT, 'issue', 'edit');
+
+router::controller(IssueController::class)
+	->addAction(request::HTTP_DELETE, 'issue', 'delete');
