8ee92add1257afdb31d4e99d669ab7275d6e4e44
Your Name <you@example.com>
2017-03-17 13:37:30 +0200

    sql ffedback tables

--- sql/db_issue.sql
+++ sql/db_issue.sql
@@ -55,3 +55,27 @@ CREATE TABLE IF NOT EXISTS `issue` (
 	INDEX `date_added` (`date_added`)	
 
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `feedback_issue` (
+  `issue_id` int(11) NOT NULL AUTO_INCREMENT,
+  `helpful` tinyint(1) NOT NULL,
+  `member_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `date_added` DATETIME NOT NULL,
+
+  PRIMARY KEY (`issue_id`),
+  INDEX `member_id` (`member_id`),
+  INDEX `helpful` (`helpful`),
+  INDEX `date_added` (`date_added`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `feedback_issue_comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `helpful` tinyint(1) NOT NULL,
+  `member_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `date_added` DATETIME NOT NULL,
+
+  PRIMARY KEY (`comment_id`),
+  INDEX `member_id` (`member_id`),
+  INDEX `helpful` (`helpful`),
+  INDEX `date_added` (`date_added`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
